86819396cfbab567f54e254208733cea1c07fbe2
Elle <elli.g.pope@gmail.com>
2017-10-26 14:02:53 -0700

    created a very simple function that resets the audioPlayer's start time to 0 when called and then plays the sound. Gives the wonderful effect of 'playing all the notes' when hovered across.

--- audio.js
+++ audio.js
@@ -1,6 +1,7 @@
-    function sound(surl) {
-        document.getElementById("hidden").innerHTML =
-        "<embed src="+surl+" hidden=true autostart=true loop=false>";
-    }
-
-/* this is not my code. I found this on this forum --> http://stackoverflow.com/questions/5748174/playing-sound-file-onmouseover-event*/
+function playAudio(source){
+	var audioPlayer = source;
+	
+	audioPlayer.currentTime = 0;
+	audioPlayer.play();
+	
+}

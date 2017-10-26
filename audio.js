a7a1ce156588b3522723fe3bebc49d8dec8a16e7
Elle <elli.g.pope@gmail.com>
2017-10-26 13:20:03 -0700

    old files from previous xylophone project Will work on making them web friendly.

---
+++ audio.js
@@ -0,0 +1,6 @@
+    function sound(surl) {
+        document.getElementById("hidden").innerHTML =
+        "<embed src="+surl+" hidden=true autostart=true loop=false>";
+    }
+
+/* this is not my code. I found this on this forum --> http://stackoverflow.com/questions/5748174/playing-sound-file-onmouseover-event*/
